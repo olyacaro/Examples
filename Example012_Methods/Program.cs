74aec5db566683ec3934bc43b236223703ef6867
Olga <olyacaro@mail.ru>
2023-09-14 01:58:46 +0300

    add Упорядочить массив – это как?

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -118,8 +118,44 @@ string Replace(string text, char oldValue, char newValue) //Replace - услов
     return result;
 }
 
-string newText = Replace(text, ' ', '|');
-Console.WriteLine(newText);
-Console.WriteLine();
-newText = Replace(newText, 'к', 'К'); //Заменили символы.
-Console.WriteLine(newText);
+//string newText = Replace(text, ' ', '|');
+//Console.WriteLine(newText);
+//Console.WriteLine();
+//newText = Replace(newText, 'к', 'К'); //Заменили символы.
+//Console.WriteLine(newText);
+
+// Упорядочить массив – это как?
+
+int[] arr = {1,5,4,3,2,6,7,1,1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for(int j = i + 1 ; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
