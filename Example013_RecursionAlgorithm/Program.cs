fa52d486cf1140160bf2183883aad07b7687a57e
Olga <olyacaro@mail.ru>
2023-09-24 16:30:46 +0300

    add Фибоначчи

--- Example013_RecursionAlgorithm/Program.cs
+++ Example013_RecursionAlgorithm/Program.cs
@@ -134,3 +134,24 @@ Console.WriteLine($"{i}! = {Factorial(i)}");
 }
 
 */
+
+/* Фибоначчи
+1 1 2 3 5 8 13 21 ...
+fn = fn-1 + fn-2
+
+f(1) = 1
+f(2) = 1
+f(n) = f(n-1) + f(n-2)
+*/
+/*
+int Fibonacci(int n)
+{
+    if(n == 1 || n ==2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for(int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
+*/
